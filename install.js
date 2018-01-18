24fe4aac804dde8a094770bd3429a4ddb81915c2
Darren Mackintosh <takuhii@gmail.com>
2018-01-18 14:58:57 +0000

    Update install.js

--- install.js
+++ install.js
@@ -301,7 +301,7 @@ function copyIntoPlace(extractedPath, targetPath) {
     var files = fs.readdirSync(extractedPath)
     for (var i = 0; i < files.length; i++) {
       var file = path.join(extractedPath, files[i])
-      if (fs.statSync(file).isDirectory()) {
+      if (fs.statSync(file).isDirectory() && file.indexOf(helper.version) != -1) {
         console.log('Copying extracted folder', file, '->', targetPath)
         return kew.nfcall(fs.copySync(file, targetPath))
       }
